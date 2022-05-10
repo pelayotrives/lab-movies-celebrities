7018443983c646dd792d359c8253c627aa96cdd3
Pelayo Trives <pelayotrives@gmail.com>
2022-05-10 16:12:54 +0200

    Iteración 2.

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,17 @@
-//  Add your code here
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose
+
+const celebritySchema = new Schema ({
+    name: {
+        type: String
+    },
+    occupation: {
+        type: String
+    },
+    catchPhrase: {
+        type: String
+    }
+})
+
+const ModelCelebrities = model("model", celebritySchema);
+module.exports = ModelCelebrities
