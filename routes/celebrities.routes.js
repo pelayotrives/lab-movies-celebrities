14c12f2125cb5f29492cd0ca39a499999094770a
Ana Sobrino <anasonu@hotmail.com>
2022-05-10 16:41:11 +0200

    Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,30 @@
 const router = require("express").Router();
+const ModelCelebrities = require("../models/Celebrity.model.js")
 
-//*
+
+// GET "/celebrities/create" => Creae nuevas celebrities
+router.get("/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity.hbs");
+})
+
+//POST "/celebrities/create" => Enviar información de formulario
+
+router.post("/create", (req, res, next) => { 
+    const {name, occupation, catchPhrase} = req.body;
+    ModelCelebrities.create({
+        name,
+        occupation,
+        catchPhrase
+    })
+    .then((response) => {
+        res.redirect("/celebrities")
+    })
+    .catch((err) => {
+        res.render("celebrities/new-celebrity.hbs")
+        console.log(err);
+    })
+})
+
+
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,6 @@ const moviesRoutes = require("./movies.routes.js")
 router.use("/movies", moviesRoutes)
 
 const celebritiesRoutes = require("./celebrities.routes.js")
-router.use("/celebrities", moviesRoutes)
+router.use("/celebrities", celebritiesRoutes)
 
 module.exports = router;
